b43f9e5e977eaba758c6cf3aae92fef3056eb9b1
Carlos Mecha <mecha.carlos@gmail.com>
2017-01-19 15:30:01 -0800

    Add the script

--- README.md
+++ README.md
@@ -1,2 +1,29 @@
-# dropbox-uploader
-A python script to upload individual files to your personal dropbox account
+# Dropbox Uploader
+
+A python script for uploading files to your dropbox account.
+
+This script is based on a [python sdk example](https://github.com/dropbox/dropbox-sdk-python/blob/master/example/back-up-and-restore/backup-and-restore-example.py).
+
+## Requirements
+
+- Python 2.7
+- Dropbox Python SDK
+
+## Configuration
+
+A token must be provided as environment variable:
+```bash
+$ DROPBOX_TOKEN='my_token'
+```
+
+You can get your personal token from [here](https://blogs.dropbox.com/developers/2014/05/generate-an-access-token-for-your-own-account/).
+
+An optional remote path can be set up defining `DROPBOX_PATH` env variable.
+
+## Run it
+
+```bash
+$ python dropbox-uploader.py my-file.txt
+```
+
+
---
+++ dropbox-uploader.py
@@ -0,0 +1,65 @@
+
+import dropbox
+import dropbox.files
+import dropbox.exceptions
+import logging
+import os
+import os.path
+import sys
+
+logging.basicConfig(level=logging.INFO)
+
+def backup(dbx, file_name, remote_path = None, logger = None):
+    """
+    Backups a file into the dropbox folder.
+    """
+
+    path = remote_path if remote_path else '/';
+    backup_file_name = os.path.join(path, file_name)
+
+    with open(file_name, 'rb') as fd:
+
+        if logger:
+            logger.debug('Uploading %s to Dropbox as %s', file_name, backup_file_name)
+
+        try:
+            dbx.files_upload(fd.read(), backup_file_name, mode = dropbox.files.WriteMode('overwrite'))
+        except dropbox.exceptions.ApiError as err:
+            if err.error.is_path() and err.error.get_path().error.is_insufficient_space():
+                if logger:
+                    logger.error('Insufficient space')
+            elif err.user_message_text:
+                if logger:
+                    logger.error('Dropbox error %s', err.user_message_text)
+            else:
+                if logger:
+                    logger.error('Dropbox error %s', err)
+            return False
+        return True
+
+if __name__ == '__main__':
+
+    logger = logging.getLogger(__name__)
+    token = os.getenv('DROPBOX_TOKEN', '')
+    remote_path = os.getenv('DROPBOX_PATH', '/')
+
+    if len(sys.argv) < 2:
+        logger.error('The file name must be provided')
+        sys.exit(1)
+
+    if len(token) == 0:
+        logger.error('The token hasn\'t been provided.')
+        sys.exit(1)
+
+    logger.debug("Creating a Dropbox object")
+    dbx = dropbox.Dropbox(token)
+
+    try:
+        dbx.users_get_current_account()
+    except dropbox.exceptions.AuthError as err:
+        logger.error('Invalid access token')
+        sys.exit(2)
+
+    res = backup(dbx, sys.argv[1], remote_path, logger)
+    sys.exit(0 if res else 1)
+
